6dbcf8e4af2a64c95ad314c4631d1fd1002eaeb0
Arerspx <federicowisely88@gmail.com>
2025-11-03 01:28:51 +0700

    Update Kursi.java

--- Kursi.java
+++ Kursi.java
@@ -1,3 +1,27 @@
 public class Kursi {
-    
+    private String nomorKursi;
+    private boolean tersedia;
+    private Film film;
+
+    public Kursi(String nomorKursi, Film film) {
+        this.nomorKursi = nomorKursi;
+        this.film = film;
+        this.tersedia = true;
+    }
+
+    public String getNomorKursi() {
+        return nomorKursi;
+    }
+
+    public boolean isTersedia() {
+        return tersedia;
+    }
+
+    public void setTersedia(boolean tersedia) {
+        this.tersedia = tersedia;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
 }
